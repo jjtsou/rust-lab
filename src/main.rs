1fe566653c2cea0b7491827e1cebcc17bb32731c
jjtsou <itsourtis@gmail.com>
2024-08-08 23:05:32 +0300

    implement deal function

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,17 @@ impl Deck {
          self.cards.shuffle(&mut rng);
     }
 
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+
 }
 
 fn main() {
     let mut deck: Deck = Deck::new();
-    deck.shuffle();
-
-    println!("here is your deck: {:#?}", deck.cards);
+    // deck.shuffle();
+    let cards_on_hand = deck.deal(3);
+    
+    println!("here is your deck: {:#?}", deck);
+    println!("here is your hand: {:#?}", cards_on_hand);
 }
